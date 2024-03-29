1c11bc1015be6a5fcbbd2c5132c185cf2b6489dc
Glorie Ramazani <glorieramazani@gmail.com>
2024-03-28 21:58:24 -0700

    Implement the addition of a person to the phonebook

--- fullstack-open/part2/phonebook/src/App.jsx
+++ fullstack-open/part2/phonebook/src/App.jsx
@@ -6,19 +6,34 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleChange = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPersons = persons
+    newPersons.push({name: newName})
+    // console.log(newPersons)
+    setPersons(newPersons)
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {/* <div>debug: {newName}</div> */}
+      <div>{persons.map( (person, index) => <div key={index}>{person.name}</div>)}</div>
     </div>
   )
 }
