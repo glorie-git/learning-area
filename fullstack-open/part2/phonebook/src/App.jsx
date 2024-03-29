2af594fe4e2340664cebdcb504023ef612d3ca53
Glorie Ramazani <glorieramazani@gmail.com>
2024-03-28 23:24:38 -0700

    Implement a search field that can be used to filter the list of people by name

--- fullstack-open/part2/phonebook/src/App.jsx
+++ fullstack-open/part2/phonebook/src/App.jsx
@@ -3,16 +3,52 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [filterOn, setFilterOn] = useState(false)
+  const [filteredPersons, setFilteredPersons] = useState([])
 
   const handleChange = (event) => {
     // console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    const newFilter = event.target.value
+    setFilter(newFilter)
+    
+    // on input change, check if field is empty
+    // if empty then we do not filter
+    if (event.target.value.length > 0){
+      // console.log(newFilter)
+
+      // state filter state so we display filtered array
+      setFilterOn(true)
+
+      // loop through persons object and filter on objects with names
+      // that include the string of value newFilter in it 
+      const newPersons = persons.filter((person) => {
+        // toLowerCase makes our search icase insensitive
+        return person.name.toLowerCase().includes(newFilter)
+      })
+
+      // pass new filtered list
+      setFilteredPersons(newPersons)
+      // console.log(newPersons)
+
+    } else {
+
+      // if there are no characters on filter input field then set filter off and display default phone book
+      setFilterOn(false)
+      setFilteredPersons([])
+    }
   }
 
   const handleNewNumber = (event) => {
@@ -45,20 +81,24 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNewNumber}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {/* <div>debug: {newName}</div> */}
-      <div>{persons.map( (person) => <div key={person.name}>{person.name} {person.number}</div>)}</div>
+      <div>
+        {filterOn ? 
+          filteredPersons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)
+          : persons.map( (person) => <div key={person.name}>{person.name} {person.number}</div>)
+        }
+      </div>
     </div>
   )
 }
