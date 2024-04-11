0f9b5d8fc4f5ff780cf739ae5925c216dc5f8653
Glorie Ramazani <glorieramazani@gmail.com>
2024-04-10 21:28:17 -0700

    Make component key 'person.id' instead of 'person.name'

--- fullstack-open/part2/phonebook/src/components/Persons.jsx
+++ fullstack-open/part2/phonebook/src/components/Persons.jsx
@@ -4,7 +4,7 @@ const Persons = (props) => {
     // persons.map( (person) => <Person key={person.name} person={person}/>)
     // console.log(props)
     return (
-      props.persons.map( (person) => <Person key={person.name} person={person}/>)
+      props.persons.map( (person) => <Person key={person.id} person={person}/>)
     )
   }
 
