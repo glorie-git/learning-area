dc083ece1f9c1a6f51cf164cb7b4cb76b3494c43
Glorie Ramazani <glorieramazani@gmail.com>
2024-04-14 22:27:14 -0700

    Add main functionality of fetch currency data from the er-api API and displaying the response

--- fullstack-open/part2/currency_exchange/src/App.jsx
+++ fullstack-open/part2/currency_exchange/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("")
+  const [currency, setCurrency] = useState(null)
+  const [rates, setRates] = useState({})
+
+  useEffect ( () => {
+    console.log('in useEffect')
+
+    if (currency) {
+      axios
+      .get(`https://open.er-api.com/v6/latest/${currency}`)
+      .then( response => {
+        // console.log(response.data)
+        setRates(response.data.rates)
+      })
+    }
+
+    console.log('end of useEffect')
+  }, [currency])
+
+  const handleChange = (event) => {
+    // console.log(event.target.value)
+    setValue(event.target.value)
+  }
+
+  const onSearch = (event) => {
+    event.preventDefault()
+    setCurrency(value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <form onSubmit={onSearch}>
+      currency:
+      <input value={value} onChange={handleChange}/>
+      <button type="submit">exchange rate</button>
+    </form>
+    <pre>
+      {JSON.stringify(rates, null, 2)}
+    </pre>
     </>
   )
 }
