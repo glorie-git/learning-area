009b82e9d43649f7dd55da3e36fc855ce1f57d5f
Glorie Ramazani <glorieramazani@gmail.com>
2024-06-06 08:49:04 -0700

    Add error handling middleware and backend contact updating endpoint.

--- fullstack-open/apps/phonebook/backend/index.js
+++ fullstack-open/apps/phonebook/backend/index.js
@@ -21,43 +21,47 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-      response.json(people);
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+    .then(people => {
+      if (people) {
+        response.json(people);
+      } else {
+        response.status(404).end();
+      }
     })
+    .catch(error => next(error));
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = phonebook.find (person => Number(person.id) === id);
-    if (person) {
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person){
         response.json(person);
-    } else {
+      } else {
         response.status(404).end();
-    }
+      }
+    })
+    .catch ( error => next(error))  
+  }
+)
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  // id has a curly brace at the end for some reason
+  const id = request.params.id;
+
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -74,6 +78,9 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
+  // Is this an existing user?
+  // console.log(Person.findOneAndUpdate({ name: body.name }, { number: body.number }));
+
   const person = Person({
     name: body.name,
     number: body.number,
@@ -86,9 +93,11 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    phonebook = phonebook.filter (person => Number(person.id) !== id);
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
     response.status(204).end();
+  })
+  .catch(error => next(error));
 })
 
 const PORT = 3001
@@ -104,4 +113,17 @@ app.get('/info', (request, response) => {
     response.send(content);
 })
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error);
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
--- fullstack-open/apps/phonebook/backend/models/phonebook.js
+++ fullstack-open/apps/phonebook/backend/models/phonebook.js
@@ -4,15 +4,16 @@ mongoose.set('strictQuery',false);
 
 const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url);
+console.log('Connecting to MongoDB...'); //, url);
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+      console.log('Connected to MongoDB!')
+  })
+  .catch(error => {
+      console.log('Error connecting to MongoDB:', error.message)
+  }
+)
 
 const personSchema = new mongoose.Schema({
     name: String,
