427402fe7971f654768b80d6cc8322b4710bf9f1
Glorie Ramazani <glorieramazani@gmail.com>
2024-07-03 18:22:49 -0700

    Configure eslint.

--- fullstack-open/apps/phonebook/backend/eslint.config.mjs
+++ fullstack-open/apps/phonebook/backend/eslint.config.mjs
@@ -1,17 +1,22 @@
+import globals from "globals";
 import stylisticJs from '@stylistic/eslint-plugin-js'
 import js from '@eslint/js'
-
 
 export default [
   js.configs.recommended,
   {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
     plugins: {
       '@stylistic/js': stylisticJs
     },
-    ignores: ['dist'],
     rules: {
-      'no-unused-vars': 'error',
-      'no-undef': 'error',
       '@stylistic/js/indent': [
         'error',
         2
@@ -34,8 +39,12 @@ export default [
         'error', 'always'
       ],
       'arrow-spacing': [
-        'error', { 'before': true, 'after': true }
-      ]
-    }
-  }
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
+    },
+  },
+  { 
+    ignores: ["dist/**", "build/**"],
+  },
 ]
