5da6059d4c23a8e74ab0434e1e2389134366049b
Glorie Ramazani <glorieramazani@gmail.com>
2024-07-05 14:19:59 -0700

    Install cors and organize code.

--- fullstack-open/apps/phonebook/backend/index.js
+++ fullstack-open/apps/phonebook/backend/index.js
@@ -5,34 +5,32 @@ const mongoose = require('mongoose')
 const phonebookRouter = require('./controllers/phonebook')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const cors = require('cors')
 
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-app.use(express.static('dist'))
-
 logger.info('Connecting to MongoDB...')
 
 mongoose.set('strictQuery',false)
-
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('Connected to MongoDB!')
   })
   .catch(error => {
     logger.error('Error connecting to MongoDB:', error.message)
-  }
-  )
+  })
 
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/phonebook/', phonebookRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}.`)
 })
-
-app.use(middleware.unknownEndpoint)
-// This has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(middleware.errorHandler)
