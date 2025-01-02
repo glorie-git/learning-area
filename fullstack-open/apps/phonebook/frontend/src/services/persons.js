973515532a64fa23780934f81ffea150691ba74b
Glorie Ramazani <glorieramazani@gmail.com>
2025-01-02 08:00:21 -0800

    Update Phonebook frontend

--- fullstack-open/apps/phonebook/frontend/src/services/persons.js
+++ fullstack-open/apps/phonebook/frontend/src/services/persons.js
@@ -1,24 +1,24 @@
-import axios from "axios"
-const baseUrl = '/api/persons'
+import axios from "axios";
+const baseUrl = "/api/phonebook";
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const create = ({name, number}) => {
-    const request = axios.post(baseUrl, {name, number})
-    return request.then(response => response.data)
-}
+const create = ({ name, number }) => {
+  const request = axios.post(baseUrl, { name, number });
+  return request.then((response) => response.data);
+};
 
 const deletePerson = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
-}
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 
 const updatePerson = (id, updatedPerson) => {
-    const request = axios.put(`${baseUrl}/${id}`, updatedPerson)
-    return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, updatedPerson);
+  return request.then((response) => response.data);
+};
 
-export default { getAll, create, deletePerson, updatePerson}
+export default { getAll, create, deletePerson, updatePerson };
